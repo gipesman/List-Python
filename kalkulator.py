c75815f5d7632b98191e38fc499d237062f70565
gipesman <37566237+gipesman@users.noreply.github.com>
2018-04-16 14:59:21 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,22 @@
+Tambah, Kurang, Kali, Bagi = range(4)
+
+def hitung(a, b, operator=Tambah, frmtOut=float):
+    hasil = 0
+    if operator == Kurang:
+        hasil = a-b
+    elif operator == Tambah:
+        hasil = a+b
+    elif operator == Kali:
+        hasil = a*b
+    elif operator == Bagi:
+        hasil = a/b
+    else:
+        print("Operator yang dijalankan Tambah, Kurang, Kali, Bagi")
+
+    if frmtOut == float:
+        hasil = float(hasil)
+    elif frmtOut == int:
+        hasil = round(hasil)
+    else:
+        ValueError("Format yang di jalankan yaitu Int Atau Float")
+    return hasil
